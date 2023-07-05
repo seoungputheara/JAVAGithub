bc967dcf0e92729e6172305b1ddf3e6962265cea
seoungputheara <138677726+seoungputheara@users.noreply.github.com>
2023-07-05 18:48:45 +0700

    FirstCM
    javacodeFX

---
+++ js/web.js
@@ -0,0 +1,8 @@
+const hambugarBtn = document.querySelector(".hambugar");
+const Menubar = document.querySelector(".Responsive-nav");
+
+hambugarBtn.addEventListener("click", function(){
+    Menubar.classList.toggle('active');
+});
+
+
